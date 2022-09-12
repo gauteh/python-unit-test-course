7c71ff59639a5aed3d80d37d5fa978f6468d22ab
Gaute Hope <gaute.hope@met.no>
2022-09-12 12:47:57 +0200

    init

---
+++ optional_plotting.py
@@ -0,0 +1,10 @@
+## tests/conftest.py
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption('--plot', action='store_true', help='Show plots', default=False)
+
+
+@pytest.fixture
+def plot(pytestconfig):
+    return pytestconfig.getoption('plot')
