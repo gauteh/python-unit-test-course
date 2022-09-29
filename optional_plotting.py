48f32c1010cddab06fbaff5afd9836ff6dda2eab
Gaute Hope <gaute.hope@met.no>
2022-09-29 12:21:58 +0200

    opt plot

--- optional_plotting.py
+++ optional_plotting.py
@@ -8,3 +8,18 @@ def pytest_addoption(parser):
 @pytest.fixture
 def plot(pytestconfig):
     return pytestconfig.getoption('plot')
+
+## In your test
+import xarray as xr
+import matpotlib.pyplot as plt
+
+def test_read_signal(plot):
+    d = xr.open_dataset('somedata.nc')
+    assert len(d.z) == 1024
+
+    if plot:
+        plt.figure()
+        plt.plot(d.z)
+        plt.show()
+
+
